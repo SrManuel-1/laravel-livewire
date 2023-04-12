4b022b2ed4f5a3085494792f6c5d7d2751cf0c1b
SrManuel-1 <stevenlievano7575@gmail.com>
2023-04-12 06:19:05 -0500

    Transformación de variables, en el componente

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -6,7 +6,11 @@ use Livewire\Component;
 
 class ShowPosts extends Component
 {
-    public $title;//Llamamos la variable inicializada en componente del dashboard para que pueda ser usada
+    public $titulo;
+
+    public function mount($title){//se define un metodo llamado mount
+        $this->titulo = $title; //Se le asigna esta informacion a esta propiedad
+    }
     public function render()
     {
         return view('livewire.show-posts');
